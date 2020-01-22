5c27b7d415af27301846de6ad1b13acfb00413d0
jjao <joanna.i.jao@gmail.com>
2020-01-21 16:58:58 -0800

    submit w6d1 classwork

---
+++ W6D1/build-own-activerecord/lib/01_sql_object.rb
@@ -0,0 +1,65 @@
+require_relative 'db_connection'
+require 'active_support/inflector'
+# NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
+# of this project. It was only a warm up.
+
+class SQLObject
+  def self.columns
+    arr = DBConnection.execute2(<<-SQL)
+      SELECT
+        *
+      FROM
+        #{self.table_name}
+    SQL
+
+    arr[0].map {|el| el.to_sym}
+  end
+
+  def self.finalize!
+  end
+
+  def self.table_name=(table_name)
+    @table_name = table_name
+  end
+
+  def self.table_name
+    @table_name ||= "#{self}".tableize
+  end
+
+  def self.all
+    # ...
+  end
+
+  def self.parse_all(results)
+    # ...
+  end
+
+  def self.find(id)
+    # ...
+  end
+
+  def initialize(params = {})
+    # ...
+  end
+
+  def attributes
+    hash = Hash.new()
+    self.columns.each { |col| hash[col] =  }
+  end
+
+  def attribute_values
+    # ...
+  end
+
+  def insert
+    # ...
+  end
+
+  def update
+    # ...
+  end
+
+  def save
+    # ...
+  end
+end
