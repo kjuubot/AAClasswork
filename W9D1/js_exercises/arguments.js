7c8ad65c969c8bfe678163d41cd3d8894a0d9e14
jjao <joanna.i.jao@gmail.com>
2020-02-10 17:42:55 -0800

    add w9d1 classwork

---
+++ W9D1/js_exercises/arguments.js
@@ -0,0 +1,70 @@
+function sum(){
+    let sum = 0;
+    let args = Array.prototype.slice.call(arguments);
+    // let args= [...arguments];
+    args.forEach(el => {
+        sum += el;
+    });
+    console.log(sum);
+}
+
+// sum(1, 2, 3, 4);
+// sum(1, 2, 3, 4, 5)
+
+// myBind(this, arguments of function we're binding) is a function
+
+Function.prototype.myBind = function(context) {
+    let that = this;
+    let args = [...arguments];
+    console.log(arguments);
+
+    return function() {
+        args.push([...arguments])
+        return that.apply(context, args);
+    };
+}
+
+class Cat {
+    constructor(name) {
+        this.name = name;
+    }
+
+    says(sound, person) {
+        console.log(`${this.name} says ${sound} to ${person}!`);
+        return true;
+    }
+}
+
+class Dog {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const markov = new Cat("Markov");
+const pavlov = new Dog("Pavlov");
+
+markov.says("meow", "Ned");
+// Markov says meow to Ned!
+// true
+
+// bind time args are "meow" and "Kush", no call time args
+markov.says.myBind(pavlov, "meow", "Kush")();
+// Pavlov says meow to Kush!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "a tree"
+markov.says.myBind(pavlov)("meow", "a tree");
+// Pavlov says meow to a tree!
+// true
+
+// bind time arg is "meow", call time arg is "Markov"
+markov.says.myBind(pavlov, "meow")("Markov");
+// Pavlov says meow to Markov!
+// true
+
+// no bind time args (other than context), call time args are "meow" and "me"
+const notMarkovSays = markov.says.myBind(pavlov);
+notMarkovSays("meow", "me");
+// Pavlov says meow to me!
+// true
